6f0e2cf169b7ad309e770fa27484a725125cf1ce
Matt Im <matthew.seiim@gmail.com>
2020-03-09 21:24:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-
+def batch_badge_creator(attendees)
+  messages = []
+  messages << attendees
+  return "Hello, my name is #{name}."
 end
 
 def assign_rooms
