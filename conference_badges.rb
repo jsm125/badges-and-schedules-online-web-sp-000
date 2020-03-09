cf8bd80849db9e9c5d4384aa93eae23c916d8cd0
Matt Im <matthew.seiim@gmail.com>
2020-03-09 22:03:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,9 @@ end
 
 def assign_rooms(speakers)
   assign = []
-  
+  speakers.each_with_index{|speaker, index|}
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    return assign
 end
 
 def printer
