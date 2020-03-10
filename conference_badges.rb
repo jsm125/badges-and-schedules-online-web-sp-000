b6c5d716c7b46458da88c27de326fcd03caa9962
Matt Im <matthew.seiim@gmail.com>
2020-03-10 01:04:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each do|badge|
+  batch_badge_creator(attendees).each do|badge|
     puts badge
   end
   
